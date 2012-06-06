bdcf5301f6464932c5fd8c5b70a39131f7d2fccf
Christopher <c.freden@gmail.com>
2012-06-07 01:07:41 +0200

    Added bookmarkable documents

--- coffee/script.coffee
+++ coffee/script.coffee
@@ -1,6 +1,18 @@
 markdown = new Showdown.converter()
 
 $(document).ready ->
+  serializeToUrl = ->
+    "#{location.protocol}//#{location.host}#{location.pathname}#?#{btoa editor.getValue()}"
+
+  docTitle = ->
+    e = $('<div></div>').append $('#view h1').first().html()
+    $('.index', e).remove()
+    e.text() or 'untitled'
+
+  updateStatus = ->
+    location.hash = '#?'+btoa editor.getValue() if btoa?
+    document.title = "Dr. Markdown - #{docTitle()}"
+
   updateToc = -> $('#toc').html $('#view').toc()
 
   updateIndex = -> $('#view').number().index()
@@ -14,17 +26,15 @@ $(document).ready ->
 
   if BlobBuilder?
     $('#download').click ->
-      filename = $('#view h1').first().text() or 'untitled'
       bb = new BlobBuilder
       bb.append editor.getValue()
-      saveAs bb.getBlob('text/plain;charset=utf-8'), filename+'.md'
+      saveAs bb.getBlob('text/plain;charset=utf-8'), docTitle()+'.md'
   else
     $('#download').hide()
 
   if btoa?
     $('#link-b64').click ->
-      url = "#{location.protocol}//#{location.host}#{location.pathname}#?#{btoa editor.getValue()}"
-      $('#link-b64-text').val(url)
+      $('#link-b64-text').val(serializeToUrl())
       .removeClass('hidden')
       .focus().select()
       .blur -> $(@).addClass('hidden')
@@ -49,19 +59,27 @@ $(document).ready ->
     editor.setOption 'mode', 'gfm'
     $('#modes .label').removeClass('active')
     $(@).addClass('active')
-  $('#mode-css').click ->
-    editor.setOption 'mode', 'css'
+  $('#mode-html').click ->
+    editor.setOption 'mode', 'htmlmixed'
     $('#modes .label').removeClass('active')
     $(@).addClass('active')
 
   editor = CodeMirror.fromTextArea $('#input-md')[0],
     mode: 'gfm'
-    theme: 'neat'
+    theme: 'default'
     lineNumbers: no
     lineWrapping: yes
     onChange: updateView
+    onBlur: updateStatus
+    onDragEvent: (editor, event) ->
+      $('#drag-n-drop-wrap').remove() if event.type is 'drop'
+      false
 
   if atob? and location.hash?.substr(0,2) is '#?'
     editor.setValue atob location.hash.substr 2
 
+  $('#drag-n-drop-wrap').removeClass 'hidden' if not editor.getValue()
+  $('#input-wrap').one 'click', -> $('#drag-n-drop-wrap').remove()
+
   updateView()
+  updateStatus()
--- js/script.js
+++ js/script.js
@@ -2,7 +2,22 @@
   var markdown;
   markdown = new Showdown.converter();
   $(document).ready(function() {
-    var editor, updateIndex, updateToc, updateView, _ref;
+    var docTitle, editor, serializeToUrl, updateIndex, updateStatus, updateToc, updateView, _ref;
+    serializeToUrl = function() {
+      return "" + location.protocol + "//" + location.host + location.pathname + "#?" + (btoa(editor.getValue()));
+    };
+    docTitle = function() {
+      var e;
+      e = $('<div></div>').append($('#view h1').first().html());
+      $('.index', e).remove();
+      return e.text() || 'untitled';
+    };
+    updateStatus = function() {
+      if (typeof btoa !== "undefined" && btoa !== null) {
+        location.hash = '#?' + btoa(editor.getValue());
+      }
+      return document.title = "Dr. Markdown - " + (docTitle());
+    };
     updateToc = function() {
       return $('#toc').html($('#view').toc());
     };
@@ -22,20 +37,17 @@
     };
     if (typeof BlobBuilder !== "undefined" && BlobBuilder !== null) {
       $('#download').click(function() {
-        var bb, filename;
-        filename = $('#view h1').first().text() || 'untitled';
+        var bb;
         bb = new BlobBuilder;
         bb.append(editor.getValue());
-        return saveAs(bb.getBlob('text/plain;charset=utf-8'), filename + '.md');
+        return saveAs(bb.getBlob('text/plain;charset=utf-8'), docTitle() + '.md');
       });
     } else {
       $('#download').hide();
     }
     if (typeof btoa !== "undefined" && btoa !== null) {
       $('#link-b64').click(function() {
-        var url;
-        url = "" + location.protocol + "//" + location.host + location.pathname + "#?" + (btoa(editor.getValue()));
-        return $('#link-b64-text').val(url).removeClass('hidden').focus().select().blur(function() {
+        return $('#link-b64-text').val(serializeToUrl()).removeClass('hidden').focus().select().blur(function() {
           return $(this).addClass('hidden');
         });
       });
@@ -66,21 +78,35 @@
       $('#modes .label').removeClass('active');
       return $(this).addClass('active');
     });
-    $('#mode-css').click(function() {
-      editor.setOption('mode', 'css');
+    $('#mode-html').click(function() {
+      editor.setOption('mode', 'htmlmixed');
       $('#modes .label').removeClass('active');
       return $(this).addClass('active');
     });
     editor = CodeMirror.fromTextArea($('#input-md')[0], {
       mode: 'gfm',
-      theme: 'neat',
+      theme: 'default',
       lineNumbers: false,
       lineWrapping: true,
-      onChange: updateView
+      onChange: updateView,
+      onBlur: updateStatus,
+      onDragEvent: function(editor, event) {
+        if (event.type === 'drop') {
+          $('#drag-n-drop-wrap').remove();
+        }
+        return false;
+      }
     });
     if ((typeof atob !== "undefined" && atob !== null) && ((_ref = location.hash) != null ? _ref.substr(0, 2) : void 0) === '#?') {
       editor.setValue(atob(location.hash.substr(2)));
     }
-    return updateView();
+    if (!editor.getValue()) {
+      $('#drag-n-drop-wrap').removeClass('hidden');
+    }
+    $('#input-wrap').one('click', function() {
+      return $('#drag-n-drop-wrap').remove();
+    });
+    updateView();
+    return updateStatus();
   });
 }).call(this);
